3429657d0b41bb4e509410634389f479142ea644
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2024-07-03 22:34:48 +0900

    [feat] Asset Setting

--- Assets/999. ThirdPartys/00. Samples/Toolbar Extender/1.4.2/Scene switcher buttons/Editor/SceneSwitcher.cs
+++ Assets/999. ThirdPartys/00. Samples/Toolbar Extender/1.4.2/Scene switcher buttons/Editor/SceneSwitcher.cs
@@ -2,92 +2,63 @@
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-
+	 
 namespace UnityToolbarExtender.Examples
 {
-	static class ToolbarStyles
-	{
-		public static readonly GUIStyle commandButtonStyle;
-
-		static ToolbarStyles()
-		{
-			commandButtonStyle = new GUIStyle("Command")
-			{
-				fontSize = 16,
-				alignment = TextAnchor.MiddleCenter,
-				imagePosition = ImagePosition.ImageAbove,
-				fontStyle = FontStyle.Bold
-			};
-		}
-	}
-
-	[InitializeOnLoad]
-	public class SceneSwitchLeftButton
-	{
-		static SceneSwitchLeftButton()
-		{
-			ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
-		}
-
-		static void OnToolbarGUI()
-		{
-			GUILayout.FlexibleSpace();
-
-			if(GUILayout.Button(new GUIContent("1", "Start Scene 1"), ToolbarStyles.commandButtonStyle))
-			{
-				SceneHelper.StartScene("ToolbarExtenderExampleScene1");
-			}
-
-			if(GUILayout.Button(new GUIContent("2", "Start Scene 2"), ToolbarStyles.commandButtonStyle))
-			{
-				SceneHelper.StartScene("ToolbarExtenderExampleScene2");
-			}
-		}
-	}
-
-	static class SceneHelper
-	{
-		static string sceneToOpen;
-
-		public static void StartScene(string sceneName)
-		{
-			if(EditorApplication.isPlaying)
-			{
-				EditorApplication.isPlaying = false;
-			}
-
-			sceneToOpen = sceneName;
-			EditorApplication.update += OnUpdate;
-		}
-
-		static void OnUpdate()
-		{
-			if (sceneToOpen == null ||
-			    EditorApplication.isPlaying || EditorApplication.isPaused ||
-			    EditorApplication.isCompiling || EditorApplication.isPlayingOrWillChangePlaymode)
-			{
-				return;
-			}
-
-			EditorApplication.update -= OnUpdate;
-
-			if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
-			{
-				// need to get scene via search because the path to the scene
-				// file contains the package version so it'll change over time
-				string[] guids = AssetDatabase.FindAssets("t:scene " + sceneToOpen, null);
-				if (guids.Length == 0)
-				{
-					Debug.LogWarning("Couldn't find scene file");
-				}
-				else
-				{
-					string scenePath = AssetDatabase.GUIDToAssetPath(guids[0]);
-					EditorSceneManager.OpenScene(scenePath);
-					EditorApplication.isPlaying = true;
-				}
-			}
-			sceneToOpen = null;
-		}
-	}
+    static class ToolbarStyles
+    {
+        public static readonly GUIStyle commandButtonStyle;
+	 
+        static ToolbarStyles()
+        {
+            commandButtonStyle = new GUIStyle("Command")
+            {
+                fontSize = 16,
+                alignment = TextAnchor.MiddleCenter,
+                imagePosition = ImagePosition.ImageAbove,
+                fontStyle = FontStyle.Bold
+            };
+        }
+    }
+	 
+    [InitializeOnLoad]
+    public class SceneSwitchLeftButton
+    {
+        static SceneSwitchLeftButton()
+        {
+            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+        }
+	      
+        static void OnToolbarGUI()
+        {
+            GUILayout.FlexibleSpace();
+	 
+            if(GUILayout.Button(new GUIContent("Awake", "Awake Scene"))) 
+            {
+                SceneHelper.OpenScene("Awake");
+            }
+	 
+            if(GUILayout.Button(new GUIContent("MainMenu", "MainMenu Scene")))
+            {
+                SceneHelper.OpenScene("MainMenu");
+            }
+	         
+            if(GUILayout.Button(new GUIContent("MainGame", "MainGame Scene")))
+            {
+                SceneHelper.OpenScene("MainGame");
+            }
+        }
+    }
+	   
+    static class SceneHelper
+    {
+        public static void OpenScene(string name)
+        {
+            var saved = EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            if (saved)
+            {
+                _ = EditorSceneManager.OpenScene($"Assets/01. Scenes/{name}.unity");
+            }
+        }
+    }
 }
