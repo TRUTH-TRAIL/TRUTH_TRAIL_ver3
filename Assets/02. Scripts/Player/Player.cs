94d89cf6d169cc47ebda4de9e5a999298b66b4f6
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2024-07-30 21:08:06 +0900

    [feat] 불필요한 Update제거

--- Assets/02. Scripts/Player/Player.cs
+++ Assets/02. Scripts/Player/Player.cs
@@ -4,11 +4,20 @@ namespace TT
 {
     public class Player : Singleton<Player>
     {
-        public bool IsCursed;
+        public bool IsCursed { get; private set; }
         public bool isAcquiredSpecialPaper;
         public bool isEqiupSpecialPaper;
 
-        public ICurse CurrentCurse;
+        private ICurse currentCurse;
+        public ICurse CurrentCurse
+        {
+            get => currentCurse;
+            set
+            {
+                currentCurse = value;
+                IsCursed = currentCurse != null;
+            }
+        }
 
         public void RemoveComponent()
         {
